f811d38b5db56a721bbf12743f3dbbbe121fff94
eric268 <46507613+eric268@users.noreply.github.com>
2021-03-28 18:32:50 -0400

    Added slow bumper, lower guides and semi circle

--- Assets/Scrips/BallPhysics.cs
+++ Assets/Scrips/BallPhysics.cs
@@ -68,20 +68,36 @@ public class BallPhysics : MonoBehaviour
             }
             else if (collision.transform.name == "top_bumper" + i)
             {
-                ChangeBallSpeed(collision, 1, true, 7.0f);
+                ChangeBallSpeed(collision, 1, true, 5.0f);
                 break;
             }
             else if (collision.transform.name == "BottomWedge" + i)
             {
-                Debug.Log("HIT");
-                ChangeBallSpeed(collision, 1, true, 5.0f);
+                Vector3 normal =  collision.contacts[0].normal;
+                Vector3 LBumperNormal = new Vector3(0.9f, 0.3f, 0.4f);
+                Vector3 RBumperNormal = new Vector3(-0.9f, 0.3f, 0.4f);
+
+                //Essentailly checking if the collision normal is equal to the normal of the hypothenus side of the bumper
+                //Since floating point percision makes it difficult to check equality between vectors I essentailly check if the distance between
+                //the vectors is extremely small aka they are the same
+                if (Vector3.SqrMagnitude(normal - LBumperNormal) < 0.01f || Vector3.SqrMagnitude(normal - RBumperNormal) < 0.01f)
+                {
+                    ChangeBallSpeed(collision, 1, true, 4.0f);
+                    Debug.LogError(collision.contacts[0].normal);
+                }
+
                 break;
             }
             else if (collision.transform.name == "TopWedge" + i)
             {
+                
                 ChangeBallSpeed(collision, 1, false, 0.0f);
                 break;
             }
+        }
+        if (collision.transform.name == "MoonBumper")
+        {
+            ChangeBallSpeed(collision, 1, true, 3.0f);
         }
     }
 }
